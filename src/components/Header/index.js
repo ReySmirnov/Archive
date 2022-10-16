c3281fa19a2b9259f47a79d1b4a9916086107320
morgan <morgan@morgan.com>
2022-10-16 21:34:35 +0500

    init

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div>
+            <div className="logo">
+                BeRemote
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/pages/main/Main.js
@@ -0,0 +1,13 @@
+import logo from '../../logo.svg';
+import './Main.css';
+import Header from "../../components/Header";
+
+function Main() {
+  return (
+      <>
+      <Header/>
+      </>
+  );
+}
+
+export default Main;
