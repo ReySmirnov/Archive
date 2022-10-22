046f71efadb17c9c357705e309de01b695bb1f12
morgan <morgan@morgan.com>
2022-10-22 19:33:12 +0500

    add page "Company"

---
+++ src/data/companies.js
@@ -0,0 +1,177 @@
+const companies = [
+  {
+    id: "safetywing",
+    title: "SafetyWing",
+    logo: "/companies/SafetyWing.png",
+    logoBackground: "rgb(92, 198, 199)",
+    shortDescription:
+      "SafetyWing is building the first global safety net for remote companies, remote workers and nomads worldwide",
+    description:
+      "SafetyWing is building the first global safety net for remote companies, remote workers and nomads worldwide. We’re doing this by launching a fleet of global products — Remote Health insurance, Remote Doctor, Remote Pensions and more.",
+    techStack: [],
+    workCulture: ["Work from anywhere", "Flexible hours", "Company retreats"],
+    benefits: [
+      "Equity",
+      "Health insurance",
+      "Hardware budget",
+      "Continuing education stipend",
+    ],
+    url: "safetywing.com",
+    tags: [],
+  },
+  {
+    id: "contra",
+    title: "Contra",
+    logo: "/companies/contra.svg",
+    shortDescription:
+      "Our mission fuels our independent community and global team to transform future of work",
+    description:
+      "Our mission fuels our independent community and global team to transform future of work",
+    techStack: [],
+    workCulture: ["Work from anywhere", "Company retreats"],
+    benefits: ["Equity", "Health insurance", "Hardware budget"],
+    url: "contra.com",
+    tags: [],
+  },
+  {
+    id: "toggl",
+    title: "Toggl",
+    logo: "/companies/toggl.png",
+    shortDescription:
+      "Toggl is a triple threat of productivity tools that helps teams do their best work by being: Effective, Empowering, Effortless",
+    description:
+      "Toggl is a triple threat of productivity tools that helps teams do their best work by being:\n\nEffective\nAutomating soul-destroying admin so you can focus your head and heart on other stuff\n\nEmpowering\nThink powerful project management, not spyware or micromanagement\n\nEffortless\nNo steep learning curves here; onboarding is as simple as a few clicks",
+    techStack: [],
+    workCulture: ["Work from anywhere", "Flexible hours", "Company retreats"],
+    benefits: [
+      "Hardware budget",
+      "Home office budget",
+      "Coworking reimbursement",
+      "Continuing education stipend",
+    ],
+    url: "toggl.com",
+    tags: [],
+  },
+  {
+    id: "helpscout",
+    title: "HelpScout",
+    logo: "/companies/helpscout.svg",
+    shortDescription:
+      "Our mission is to empower businesses with tools that serve people in the most human, helpful way",
+    description:
+      "Our mission is to empower businesses with tools that serve people in the most human, helpful way.",
+    techStack: [],
+    workCulture: ["Work from anywhere", "Flexible hours", "Company retreats"],
+    benefits: [
+      "Health insurance",
+      "Dependencies health insurance",
+      "401(k) plan",
+      "Equity",
+      "Hardware budget",
+      "Home office budget",
+      "Coworking reimbursement",
+      "Continuing education stipend",
+    ],
+    url: "helpscout.com",
+    tags: [],
+  },
+  {
+    id: "hotjar",
+    title: "Hotjar",
+    logo: "/companies/hotjar.svg",
+    shortDescription:
+      "Hotjar is a successful Product Experience Insights company that operates on a fully remote basis, with team members spread throughout Europe, Africa and the Americas",
+    description:
+      "Hotjar is a successful Product Experience Insights company that operates on a fully remote basis, with team members spread throughout Europe, Africa and the Americas. Our team casts a wide net across many locations, lifestyles, and backgrounds. We celebrate the uniqueness and strength found in diversity: it’s our differences that make us interesting, and our shared belief in Hotjar’s Core Values that bind us together.\n\nThese values form Hotjar's culture as one guided by respect, transparency, collaboration, and direct feedback. We put our customers at the heart of everything we do, and we do so through a diverse team working together in an honest, inclusive environment. We all commit to creating a safe working environment and are allies to those often underrepresented - including but not limited to members of BIPOC and LGBTQIA+ communities, people with disabilities, and all people who identify as women.\n\nWe believe people from different backgrounds, with different identities and experiences, make our product and our company better – just as our users come from everywhere, a diverse team helps ensure we create a product that is accessible to a wide range of people. \n\nWe're passionate about communicating clearly with our users and within our team. We pursue big goals by prioritizing brilliantly, taking quick decisions, and delivering incremental change. Success is measured by the value we create for our users.",
+    techStack: [],
+    workCulture: ["Work from anywhere", "Flexible hours", "Company retreats"],
+    benefits: [
+      "Equity",
+      "Home office budget",
+      "Continuing education stipend",
+      "Paid parental leave",
+      "Hardware budget",
+      "Coworking reimbursement",
+    ],
+    url: "hotjar.io",
+    tags: [],
+  },
+  {
+    id: "chilipiper",
+    title: "ChiliPiper",
+    logo: "/companies/chilipiper.svg",
+    shortDescription:
+      "Our mission is to reinvent the system of action of revenue teams to provide them with new levels of productivity and job satisfaction",
+    description:
+      "Our mission is to reinvent the system of action of revenue teams to provide them with new levels of productivity and job satisfaction.\n\nOur Values\n\nHelp & Compassion\nOur #1 value. At Chili Piper, we will do everything in our power to help our team, clients, and community be successful and use compassion as the lens to see perspectives clearly.\n\nInnovate\nWe never do things simply because they have been done before or have worked for other companies. We consistently think outside the box, turn things on their heads, and create solutions that are uniquely ‘Chili Piper’.\n\nTake Ownership\nAutonomy is at the core of who we are. We trust our people to do great work from start to finish with very minimal oversight, therefore our team exercises a massive amount of ownership in their workload.\n\nGrowth Mindset\nWe are all about growth at Chili Piper. We hire talented people who are looking to accelerate their careers and partner with companies who are looking to accelerate their business.\nHave Fun\n\nWork doesn’t have to suck. We believe that people do their best when they are empowered to bring their full selves to work and enjoy the company of those doing the same. At Chili Piper, we love to laugh, be silly, and not take ourselves too seriously.",
+    techStack: ["React", "TypeScript"],
+    workCulture: [
+      "Async communications",
+      "Company retreats",
+      "Flexible hours",
+      "Work from anywhere",
+      "Few meetings",
+      "Autonomy",
+    ],
+    benefits: [
+      "Health insurance",
+      "Coworking reimbursement",
+      "Hardware budget",
+      "Home office budget",
+      "Continuing education stipend",
+      "Paid parental leave",
+      "Company credit card",
+      "Unlimited vacation",
+      "Equity",
+      "401(k) plan",
+    ],
+    url: "chilipiper.com",
+    tags: [],
+  },
+  {
+    id: "crowdcast",
+    title: "Crowdcast",
+    logo: "/companies/crowdcast.svg",
+    logoBackground: null,
+    shortDescription:
+      "Crowdcast is the online events platform for businesses, creators, and people at large to bring their community together. We've had massive growth over the past few months as people all over the world move their events online. Every month, millions of people attend events ranging from Q&As to workshops, live shows, meet-ups, webinars, online concerts, & online summits",
+    description:
+      "Crowdcast is the online events platform for businesses, creators, and people at large to bring their community together. We've had massive growth over the past few months as people all over the world move their events online. Every month, millions of people attend events ranging from Q&As to workshops, live shows, meet-ups, webinars, online concerts, & online summits. Our customers range from big companies like Adobe, IBM, Twilio, & HP to creators like Mos Def, Amanda Palmer, MIA, System of a Down & Jacob Collier.\n" +
+      "We believe technology can be used to bring people together & bridge divides. We're empowering people to share their ideas at scale, bring their communities together, and make a living in the process.  \n" +
+      "We're a profitable, remote-first, and long-term thinking company",
+    techStack: ["React", "TypeScript", "Node.js", "GraphQL"],
+    workCulture: ["Flexible hours", "Company retreats"],
+    benefits: [
+      "Equity",
+      "Health insurance",
+      "Paid parental leave",
+      "Unlimited vacation",
+      "Continuing education stipend",
+    ],
+    url: "crowdcast.io",
+    tags: [],
+  },
+  {
+    id: "chameleon",
+    title: "Chameleon",
+    logo: "/companies/chameleon.svg",
+    logoBackground: null,
+    shortDescription: "We make it easier for your users to discover value",
+    description:
+      "We make it easier for your users to discover value.\n" +
+      "We started working on Chameleon after being frustrated with sifting through docs and watching videos to learn how to use products.\n" +
+      "Our values help define who we are, how we make decisions, and what we become: Respect and Invest In Each Other, Adapt to the Circumstances, Empower You to Succeed",
+    techStack: ["React", "TypeScript", "Node.js", "GraphQL"],
+    workCulture: ["Flexible hours"],
+    benefits: [
+      "Equity",
+      "Health insurance",
+      "Continuing education stipend",
+      "Home office budget",
+    ],
+    url: "chameleon.io",
+    tags: ["saas"],
+  },
+];
+
+export default companies;
